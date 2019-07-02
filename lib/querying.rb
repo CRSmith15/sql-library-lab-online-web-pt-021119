6dcb6047c98c263de1334ed90a7202e8e7e5d018
Chance Smith <chancesmith97@hotmail.com>
2019-07-02 18:18:25 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,8 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT speices, COUNT(speices)
-  "
+  FROM characters
+  ORDER BY "
 end
 
 def select_name_and_series_subgenres_of_authors
